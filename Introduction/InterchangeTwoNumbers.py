b9d6c7b4dc7e2606420e2bf30c1a77260201f335
Prateek Mehrotra <mehrotra.prtk@gmail.com>
2015-09-23 12:52:44 -0700

    Python -Introduction

---
+++ Introduction/InterchangeTwoNumbers.py
@@ -0,0 +1,8 @@
+__author__ = 'PrateekMehrotra'
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+a = int(raw_input())
+b = int(raw_input())
+tup = (b, a)
+(x, y) = tup
+print(x)
+print(y)
